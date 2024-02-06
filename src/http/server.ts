c4b8c6c1c4d8cd2253fc5eebd054952da3b83805
oelopes <oelopes2@gmail.com>
2024-02-06 17:52:04 -0300

    feat: add vote on poll route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,12 +1,20 @@
+import fastifyCookie from '@fastify/cookie'
 import fastify from 'fastify'
 
 import { createPoll } from '../routes/create-poll'
 import { getPoll } from '../routes/get-poll-by-id'
+import { voteOnPoll } from '../routes/vote-on-poll'
 
 const app = fastify()
 
+app.register(fastifyCookie, {
+  secret: 'polls-app-unique-id-secret',
+  hook: 'onRequest',
+})
+
 app.register(createPoll)
 app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({port: 3001}).then(() => {
   console.log("RUNNING")
