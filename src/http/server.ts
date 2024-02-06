9b22a9dabb296342f5cbb1cddc1ef372be239e35
oelopes <oelopes2@gmail.com>
2024-02-06 00:15:03 -0300

    first commit

---
+++ src/http/server.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from '@prisma/client'
+import fastify from 'fastify'
+import { z } from 'zod'
+
+const app = fastify()
+
+const prisma = new PrismaClient()
+
+app.post('/polls', async (req, reply) => {
+  const createPollBody = z.object({
+    title: z.string()
+  })
+
+  const {title} = createPollBody.parse(req.body)
+
+  const poll = await prisma.poll.create({
+    data: {
+      title,
+    }
+  })
+
+  return reply.status(201).send({
+    pollId: poll.id
+  })
+})
+
+app.listen({port: 3001}).then(() => {
+  console.log("RUNNING")
+})
