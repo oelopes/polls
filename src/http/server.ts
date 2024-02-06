6b09a6ce11fa60935100fded93bd3f3e69672d4b
oelopes <oelopes2@gmail.com>
2024-02-06 16:58:39 -0300

    feat: add get poll by id route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,12 @@
 import fastify from 'fastify'
 
 import { createPoll } from '../routes/create-poll'
+import { getPoll } from '../routes/get-poll-by-id'
 
 const app = fastify()
 
 app.register(createPoll)
+app.register(getPoll)
 
 app.listen({port: 3001}).then(() => {
   console.log("RUNNING")
